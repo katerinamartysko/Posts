ad43f9a4824c7793ba7b24fc05be9a25ada5f2e8
Katerina <katerinamartysko@gmail.com>
2022-09-29 13:35:31 +0300

    add new file .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'import/prefer-default-export': 0,
+    'react/prop-types': 0,
+  },
+};
