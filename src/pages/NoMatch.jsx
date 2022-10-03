1d071746e948d2997445ca01df84981714ba108c
Katerina <katerinamartysko@gmail.com>
2022-10-03 23:34:30 +0300

    add redux for categories

--- src/pages/NoMatch.jsx
+++ src/pages/NoMatch.jsx
@@ -1,29 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ButtonCard from '../components/UI/Button/ButtonCard';
 import classes from '../components/UI/Button/ButtonCard.module.css';
-
-const cards = [
-  { id: 1, title: 'Active', isActive: true },
-  { id: 2, title: 'History', isActive: false },
-  { id: 3, title: 'Unanswered', isActive: false },
-  { id: 4, title: 'Archived', isActive: false }
-];
+import { useDispatch, useSelector } from 'react-redux';
 
 const NoMatch = () => {
-  const [activeCards, setActiveCards] = useState(cards);
+  const dispatch = useDispatch()
+  const cards = useSelector(state => state.categories.categories);
 
   const selectActive = (cardId) => {
-   const change = activeCards.map(card => {
-      card.isActive = cardId === card.id;
-
-      return card
-    });
-   setActiveCards(change)
-  };
-
+    dispatch({type: 'CHANGE_ACTIVE_CATEGORY', payload: cardId})
+    }
   return (
     <div className="card">
-      {activeCards.map(card => (
+      {cards.map(card => (
         <ButtonCard
           key={card.id}
           className={card.isActive && classes.buttonCard__active}
