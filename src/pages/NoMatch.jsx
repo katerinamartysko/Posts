fc42ec2a96da87734d230fddd7b1cb6c02cd4913
Katerina <katerinamartysko@gmail.com>
2022-10-03 21:30:22 +0300

    add logic for categories

--- src/pages/NoMatch.jsx
+++ src/pages/NoMatch.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import ButtonCard from '../components/UI/Button/ButtonCard';
+import classes from '../components/UI/Button/ButtonCard.module.css';
+
+const cards = [
+  { id: 1, title: 'Active', isActive: true },
+  { id: 2, title: 'History', isActive: false },
+  { id: 3, title: 'Unanswered', isActive: false },
+  { id: 4, title: 'Archived', isActive: false }
+];
+
 const NoMatch = () => {
-      return (
-        <div>
-          <h2 style={{color: 'red'}}>ВЫ ПЕРЕШЛИ НА НЕСУЩЕСТВУЮЩУЮ СТРАНИЦУ!</h2>
-          <p>
-            <Link to="/"> ВЕРНИТЕСЬ НА ГЛАВНУЮ СТРАНИЦУ </Link>
-          </p>
-        </div>
+  const [activeCards, setActiveCards] = useState(cards);
+
+  const selectActive = (cardId) => {
+   const change = activeCards.map(card => {
+      card.isActive = cardId === card.id;
+
+      return card
+    });
+   setActiveCards(change)
+  };
+
+  return (
+    <div className="card">
+      {activeCards.map(card => (
+        <ButtonCard
+          key={card.id}
+          className={card.isActive && classes.buttonCard__active}
+          onClick={() => selectActive(card.id)}
+        >
+          {card.title}
+        </ButtonCard>
+      ))}
+    </div>
   );
 };
 
